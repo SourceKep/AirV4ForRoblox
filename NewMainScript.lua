1c5b37177c91c7aa87ad0afc8ed9789057e5a517
SourceKep <128210845+SourceKep@users.noreply.github.com>
2023-05-21 19:31:48 -0400

    Updated Main script

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,4 +1,11 @@
-
-       loadstring(game:HttpGet("https://raw.githubusercontent.com/SourceKep/AirV4ForRoblox/main/NotificationLoading.lua", true))()
-      wait(1)
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/SourceKep/AirV4ForRoblox/main/NinjaMain.lua", true))()
+local function Checker()
+     pcall(function() 
+         if game:isloaded then 
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/SourceKep/AirV4ForRoblox/main/NotificationLoading.lua", true))()
+             elseif game:isloaded and game.PlaceId == 3956818381 then
+             loadstring(game:HttpGet("https://raw.githubusercontent.com/SourceKep/AirV4ForRoblox/main/NotificationLoading.lua", true))()
+             loadstring(game:HttpGet("https://raw.githubusercontent.com/SourceKep/AirV4ForRoblox/main/NinjaMain.lua", true))()
+       end
+   end)
+end
+Checker()
