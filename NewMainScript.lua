c044d548391d44379bb04df31d477998a51c166c
SourceKep <128210845+SourceKep@users.noreply.github.com>
2023-05-23 16:59:26 -0400

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,9 +1,54 @@
-local function Checker()
-     pcall(function() 
-         if game:isloaded then 
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/SourceKep/AirV4ForRoblox/main/NotificationLoading.lua", true))()
-             loadstring(game:HttpGet("https://raw.githubusercontent.com/SourceKep/AirV4ForRoblox/main/NinjaMain.lua", true))()
-       end
-   end)
+
+
+
+
+
+local plr = game.Players.LocalPlayer.Character.PrimaryPart or game.Players.LocalPlayer.Character:FindFirstChild("PrimaryPart")
+local RunService = game:GetService("RunService")
+
+
+  
+  
+local function Speed(Speed, Tog)
+  local Toggle = {Enabled = Tog}
+  local plr = game.Players.LocalPlayer.Character.Humanoid or game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") or game.Players.LocalPlayer.Character.PrimaryPart or game.Players.LocalPlayer.Character:FindFirstChild("PrimaryPart")
+  if plr and Toggle.Enabled == true then
+  plr.WalkSpeed = Speed
+  elseif plr and Toggle.Enabled == false then
+  while true do
+  plr.WalkSpeed = 23
+  wait()
+   end
+  end
 end
-Checker()
+
+
+local function HighJump(Toggle)
+local Toggle = {Enabled = Toggle}
+local Plr = game.Players.LocalPlayer.Character.PrimaryPart
+if Plr and Toggle.Enabled == true then
+	Plr.CFrame = Plr.CFrame + Vector3.new(0, 30, 0)
+   end
+end
+
+
+
+ 
+ 
+ local function FOVChanger(toggle, Fov)
+ local plr = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") or game.Players.LocalPlayer.Character.PrimaryPart or game.Players.LocalPlayer.Character:FindFirstChild("PrimaryPart")
+ local Toggle = {Enabled = toggle}
+ if plr and Toggle.Enabled == true then
+       while Toggle.Enabled == true do
+       game.Workspace.CurrentCamera.FieldOfView = Fov
+       wait()
+         end
+         
+         elseif plr and Toggle.Enabled == false then
+         game.Workspace.CurrentCamera.FieldOfView = 100
+       end
+    end
+ 
+ HighJump(true)
+Speed(2, false)
+FOVChanger(true, 120)
