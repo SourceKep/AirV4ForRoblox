70d550e2141d3bda7f1138a990ccc943d711ed1d
SourceKep <128210845+SourceKep@users.noreply.github.com>
2023-05-23 22:35:41 -0400

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,10 +1,13 @@
 
 
 
-
-
 local plr = game.Players.LocalPlayer.Character.PrimaryPart or game.Players.LocalPlayer.Character:FindFirstChild("PrimaryPart")
 local RunService = game:GetService("RunService")
+local TweenService = game:GetService("TweenService")
+local LightingService = game:GetService("Lighting")
+local CoreService = game:GetService("CoreGui")
+local IsInjected = true
+local load = loadstring(game:HttpGet("https://raw.githubusercontent.com/SourceKep/AirV4ForRoblox/main/NotificationLoading.lua"), true)()
 
 
   
@@ -17,7 +20,7 @@ local function Speed(Speed, Tog)
   elseif plr and Toggle.Enabled == false then
   while true do
   plr.WalkSpeed = 23
-  wait()
+  task.wait()
    end
   end
 end
@@ -32,23 +35,69 @@ if Plr and Toggle.Enabled == true then
 end
 
 
-
- 
- 
- local function FOVChanger(toggle, Fov)
+ local function FOVChanger(Fov, toggle)
  local plr = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") or game.Players.LocalPlayer.Character.PrimaryPart or game.Players.LocalPlayer.Character:FindFirstChild("PrimaryPart")
  local Toggle = {Enabled = toggle}
+
  if plr and Toggle.Enabled == true then
        while Toggle.Enabled == true do
        game.Workspace.CurrentCamera.FieldOfView = Fov
-       wait()
-         end
-         
-         elseif plr and Toggle.Enabled == false then
+       task.wait()
+   end
+ elseif Toggle.Enabled == false then
          game.Workspace.CurrentCamera.FieldOfView = 100
+     end
+end
+
+
+
+
+local function tp(child, toggle)
+local Player = game.Players.LocalPlayer.Character.PrimaryPart
+local plr2 = game.Players:FindFirstChild(child).Character.PrimaryPart
+local Tp = {Enabled = toggle}
+
+if Tp.Enabled == true and plr2 and Player then
+repeat
+  Player.CFrame = plr2.CFrame
+until plr2.Health == 0
+wait()
+ end
+end
+
+
+local function fly(Toggle)
+  local Fly = {Enabled = Toggle}
+  local plr = game.Players.LocalPlayer.Character.PrimaryPart
+  local FlyY = function()
+    local flyY = {Enabled = true}
+    if flyY.Enabled == true then
+    while flyY.Enabled == true do 
+    plr.CFrame = plr.CFrame + plr.CFrame.UpVector
+          end
        end
     end
- 
- HighJump(true)
-Speed(2, false)
-FOVChanger(true, 120)
+
+    local FlyX = function()
+      local flyX = {Enabled = true}
+      if flyX.Enabled == true then
+        while flyX.Enabled == true do
+        plr.CFrame = plr.CFrame + plr.CFrame.LookVector
+      end
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
