be19618b6b21b17e8bd3983b1d877c202ce48c78
SourceKep <128210845+SourceKep@users.noreply.github.com>
2023-06-11 12:37:30 -0400

    Update library.lua

--- library.lua
+++ library.lua
@@ -18,13 +18,6 @@ end
 
 
 
-
-
-
-
-
-
-
 function library:CreateWindow(tabl)
     local Window = Instance.new("Frame")
     local Tittle = Instance.new("TextLabel")
@@ -83,7 +76,7 @@ function library:CreateWindow(tabl)
         button.Name = "Button"
         button.Parent = Window
         button.BackgroundColor3 = Color3.fromRGB(55, 66, 218)
-        button.Position = Settings.Pos
+        button.Position = UDim2.new(-0.00254485477, 0, 0.991822243, 0)
         button.Size = UDim2.new(1, 0, 0.80590862, 0) 
         button.Font = Enum.Font.Gotham
         button.Text = Settings.Name
@@ -98,6 +91,72 @@ function library:CreateWindow(tabl)
         end)
         return Button
     end
+
+     
+    function library:Notification(Tittle, text) 
+        local Notification = Instance.new("Frame")
+        local ModuleToggle = Instance.new("TextLabel")
+        local UICorner = Instance.new("UICorner")
+        local Description = Instance.new("TextLabel")
+        local medal2 = Instance.new("ImageButton")
+        Notification.Name = "Notification"
+        Notification.Parent = Vair
+        Notification.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+        Notification.BackgroundTransparency = 0.200
+        Notification.Position = UDim2.new(0.97, 0,0.885, 0)
+        Notification.Size = UDim2.new(0.175781175, 0, 0.104368947, 0)
+        ModuleToggle.Name = "ModuleToggle"
+        ModuleToggle.Parent = Notification
+ModuleToggle.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ModuleToggle.BackgroundTransparency = 1.000
+ModuleToggle.Position = UDim2.new(0.135028511, 0, 0.0541841537, 0)
+ModuleToggle.Size = UDim2.new(0.326245666, 0, 0.364762098, 0)
+ModuleToggle.Font = Enum.Font.Gotham
+ModuleToggle.Text = Tittle
+ModuleToggle.TextColor3 = Color3.fromRGB(255, 255, 255)
+ModuleToggle.TextScaled = true
+ModuleToggle.TextSize = 25.000
+ModuleToggle.TextWrapped = true
+UICorner.Parent = Notification
+Description.Name = "Description"
+Description.Parent = Notification
+Description.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Description.BackgroundTransparency = 1.000
+Description.Position = UDim2.new(-0.0284813959, 0, 0.513164341, 0)
+Description.Size = UDim2.new(0.658695281, 0, 0.364422947, 0)
+Description.Font = Enum.Font.Gotham
+Description.Text = text
+Description.TextColor3 = Color3.fromRGB(179, 179, 179)
+Description.TextSize = 23.000
+Description.TextWrapped = true 
+medal2.Name = "medal2"
+medal2.Parent = Notification
+medal2.BackgroundTransparency = 1.000
+medal2.Position = UDim2.new(-0.000281914137, 0, -0.0175102875, 0)
+medal2.Size = UDim2.new(0, 49, 0, 45)
+medal2.ZIndex = 2
+medal2.Image = "rbxassetid://6764432408"
+medal2.ImageRectOffset = Vector2.new(150, 100)
+medal2.ImageRectSize = Vector2.new(50, 50)
+
+if game:IsLoaded() then
+local TweenService = game:GetService("TweenService")
+local TweenInformation = TweenInfo.new(0.5,Enum.EasingStyle.Sine,Enum.EasingDirection.In,0,false,0)
+local TweenInformation2 = TweenInfo.new(5,Enum.EasingStyle.Sine,Enum.EasingDirection.In,0,false,0)
+local Goals = {Position = UDim2.new(0.819963813, 0, 0.884708643, 0)} 
+local g = {Position = UDim2.new(200, 0,0, 0)} 
+local Tween = TweenService:Create(Notification, TweenInformation, Goals)
+local Tween2 = TweenService:Create(Notification, TweenInformation2, g)
+Tween:Play()
+task.wait(1)
+Tween2:Play()
+task.wait(2)
+Notification.Position = UDim2.new(200, 0,0, 0)
+Notification:Destroy()
+    end
+end
+
+
 end
 
 return library
